90aa221c7f74bb09d8d6e2477a1f5730542d950e
Marco Zagaria <marcozagaria@live.com>
2024-10-31 15:55:09 +0100

    inizio

---
+++ app_viaggi/src/main/java/marcozagaria/app_viaggi/AppViaggiApplication.java
@@ -0,0 +1,13 @@
+package marcozagaria.app_viaggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppViaggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppViaggiApplication.class, args);
+	}
+
+}
---
+++ app_viaggi/src/test/java/marcozagaria/app_viaggi/AppViaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package marcozagaria.app_viaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppViaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
